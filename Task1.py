aa2665d45ad8e9b5164d60c283877de7b186e201
VENU GOPAL RAMIREDDY <venugopalrvg9@gmail.com>
2026-02-24 19:30:39 +0530

    Add files via upload
    Assignment1 for Task 1 and Task 2 Uploaded

---
+++ Task1.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("Addition: ",addition)
+print("subtraction: ",subtraction)
+print("division: ",division)
+print("multiplication: ",multiplication)
---
+++ Task2.py
@@ -0,0 +1,4 @@
+first_name = input("Enter Your First Name: ")
+last_name  = input("Enter Your Last Name: ")
+concat = first_name +" "+last_name+'!'
+print("Hello,",concat,"Welcome to the python program.")
